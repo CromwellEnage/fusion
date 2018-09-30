66b6bcd5840d274e0040e529ca9d5d9c8442f7ca
CromwellEnage <32967088+CromwellEnage@users.noreply.github.com>
2018-09-29 23:59:20 -0400

    Adapt bimaps::relation::structured_pair
    Recognize boost::bimaps::relation::structured_pair as a Fusion sequence.

--- test/sequence/boost_compressed_pair.cpp
+++ test/sequence/boost_compressed_pair.cpp
@@ -5,92 +5,48 @@
     (See accompanying file LICENSE_1_0.txt or copy at
     http://www.boost.org/LICENSE_1_0.txt)
 ============================================================================*/
-#include <boost/core/lightweight_test.hpp>
 #include <boost/fusion/adapted/boost_compressed_pair.hpp>
-#include <boost/fusion/sequence/intrinsic/at.hpp>
-#include <boost/fusion/sequence/intrinsic/size.hpp>
-#include <boost/fusion/sequence/intrinsic/empty.hpp>
-#include <boost/fusion/sequence/intrinsic/front.hpp>
-#include <boost/fusion/sequence/intrinsic/back.hpp>
-#include <boost/fusion/container/vector/vector.hpp>
-#include <boost/fusion/container/list/list.hpp>
-#include <boost/fusion/container/generation/make_vector.hpp>
-#include <boost/fusion/container/vector/convert.hpp>
-#include <boost/fusion/sequence/comparison/equal_to.hpp>
-#include <boost/fusion/sequence/comparison/not_equal_to.hpp>
-#include <boost/fusion/sequence/comparison/less.hpp>
-#include <boost/fusion/sequence/comparison/less_equal.hpp>
-#include <boost/fusion/sequence/comparison/greater.hpp>
-#include <boost/fusion/sequence/comparison/greater_equal.hpp>
-#include <boost/fusion/mpl.hpp>
-#include <boost/fusion/support/is_view.hpp>
-#include <boost/mpl/int.hpp>
-#include <boost/mpl/is_sequence.hpp>
-#include <boost/mpl/front.hpp>
-#include <boost/mpl/back.hpp>
-#include <boost/mpl/at.hpp>
-#include <boost/mpl/assert.hpp>
-#include <boost/mpl/empty_base.hpp>
 #include <string>
+#include "adapted_pair.hpp"
+
+namespace test
+{
+    struct empty_base
+    {
+    };
+
+    bool operator==(empty_base const&, empty_base const&)
+    {
+        return true;
+    }
+
+    bool operator!=(empty_base const&, empty_base const&)
+    {
+        return false;
+    }
+}
 
 int main()
 {
     {
-        typedef boost::compressed_pair<int, std::string> pair_type;
-        BOOST_MPL_ASSERT((boost::mpl::is_sequence<pair_type>));
-        BOOST_MPL_ASSERT((
-            boost::is_same<int, boost::mpl::front<pair_type>::type>
-        ));
-        BOOST_MPL_ASSERT((
-            boost::is_same<std::string, boost::mpl::back<pair_type>::type>
-        ));
-        BOOST_MPL_ASSERT((
-            boost::is_same<int, boost::mpl::at_c<pair_type,0>::type>
-        ));
-        BOOST_MPL_ASSERT((
-            boost::is_same<std::string, boost::mpl::at_c<pair_type,1>::type>
-        ));
-        BOOST_MPL_ASSERT_NOT((boost::fusion::traits::is_view<pair_type>));
-
-        pair_type p(123, "Hola!!!");
-        BOOST_TEST(p == boost::fusion::make_vector(123, "Hola!!!"));
-
-        boost::fusion::vector<int, std::string> v(p);
-        BOOST_TEST(v == p);
-        v = p;
-        BOOST_TEST(v == p);
-
-        boost::fusion::list<int, std::string> l(p);
-        BOOST_TEST(l == p);
-        l = p;
-        BOOST_TEST(l == p);
-
-        boost::fusion::at_c<0>(p) = 6;
-        boost::fusion::at_c<1>(p) = "mama mia";
-        BOOST_TEST(p == boost::fusion::make_vector(6, "mama mia"));
-
-        BOOST_STATIC_ASSERT(
-            boost::fusion::result_of::size<pair_type>::value == 2
-        );
-        BOOST_STATIC_ASSERT(
-            !boost::fusion::result_of::empty<pair_type>::value
-        );
-
-        BOOST_TEST(boost::fusion::front(p) == 6);
-#if 0
-        BOOST_TEST(boost::fusion::back(p) == "mama mia");
-#endif
+        boost::compressed_pair<int, std::string> p(123, "Hola!!!");
+        test::pair_access_and_modify(p);
     }
 
     {
-        typedef boost::compressed_pair<int, boost::mpl::empty_base> pair_type;
+        boost::compressed_pair<short, float> p(5, 3.3f);
+        test::pair_compare(p);
+    }
+
+    {
+        typedef boost::compressed_pair<int, test::empty_base> pair_type;
         BOOST_MPL_ASSERT((boost::mpl::is_sequence<pair_type>));
         BOOST_MPL_ASSERT((
             boost::is_same<int, boost::mpl::front<pair_type>::type>
         ));
         BOOST_MPL_ASSERT((
             boost::is_same<
-                boost::mpl::empty_base
+                test::empty_base
               , boost::mpl::back<pair_type>::type
             >
         ));
@@ -99,35 +55,35 @@ int main()
         ));
         BOOST_MPL_ASSERT((
             boost::is_same<
-                boost::mpl::empty_base
+                test::empty_base
               , boost::mpl::at_c<pair_type,1>::type
             >
         ));
+        BOOST_MPL_ASSERT((
+            boost::mpl::equal_to<
+                boost::fusion::result_of::size<pair_type>
+              , boost::mpl::int_<2>
+            >
+        ));
         BOOST_MPL_ASSERT_NOT((boost::fusion::traits::is_view<pair_type>));
+        BOOST_MPL_ASSERT_NOT((boost::fusion::result_of::empty<pair_type>));
 
         pair_type p(123);
         BOOST_TEST(boost::fusion::front(p) == 123);
 
         boost::fusion::at_c<0>(p) = 6;
         BOOST_TEST(boost::fusion::front(p) == 6);
-
-        BOOST_STATIC_ASSERT(
-            boost::fusion::result_of::size<pair_type>::value == 2
-        );
-        BOOST_STATIC_ASSERT(
-            !boost::fusion::result_of::empty<pair_type>::value
-        );
     }
 
     {
         typedef boost::compressed_pair<
-            boost::mpl::empty_base
+            test::empty_base
           , std::string
         > pair_type;
         BOOST_MPL_ASSERT((boost::mpl::is_sequence<pair_type>));
         BOOST_MPL_ASSERT((
             boost::is_same<
-                boost::mpl::empty_base
+                test::empty_base
               , boost::mpl::front<pair_type>::type
             >
         ));
@@ -136,28 +92,30 @@ int main()
         ));
         BOOST_MPL_ASSERT((
             boost::is_same<
-                boost::mpl::empty_base
+                test::empty_base
               , boost::mpl::at_c<pair_type,0>::type
             >
         ));
         BOOST_MPL_ASSERT((
             boost::is_same<std::string, boost::mpl::at_c<pair_type,1>::type>
         ));
+        BOOST_MPL_ASSERT((
+            boost::mpl::equal_to<
+                boost::fusion::result_of::size<pair_type>
+              , boost::mpl::int_<2>
+            >
+        ));
         BOOST_MPL_ASSERT_NOT((boost::fusion::traits::is_view<pair_type>));
+        BOOST_MPL_ASSERT_NOT((boost::fusion::result_of::empty<pair_type>));
 
-#if 0
         pair_type p("Hola!!!");
-        BOOST_TEST(boost::fusion::front(p) == std::string("Hola!!!"));
+        BOOST_TEST(
+            p == boost::fusion::make_vector(test::empty_base(), "Hola!!!")
+        );
 
         boost::fusion::at_c<1>(p) = "mama mia";
-        BOOST_TEST(boost::fusion::at_c<1>(p) == std::string("mama mia"));
-#endif
-
-        BOOST_STATIC_ASSERT(
-            boost::fusion::result_of::size<pair_type>::value == 2
-        );
-        BOOST_STATIC_ASSERT(
-            !boost::fusion::result_of::empty<pair_type>::value
+        BOOST_TEST(
+            p == boost::fusion::make_vector(test::empty_base(), "mama mia")
         );
     }
 
@@ -176,7 +134,14 @@ int main()
         BOOST_MPL_ASSERT((
             boost::is_same<short, boost::mpl::at_c<pair_type,1>::type>
         ));
+        BOOST_MPL_ASSERT((
+            boost::mpl::equal_to<
+                boost::fusion::result_of::size<pair_type>
+              , boost::mpl::int_<2>
+            >
+        ));
         BOOST_MPL_ASSERT_NOT((boost::fusion::traits::is_view<pair_type>));
+        BOOST_MPL_ASSERT_NOT((boost::fusion::result_of::empty<pair_type>));
 
         pair_type p(456);
         BOOST_TEST(p == boost::fusion::make_vector(456, 456));
@@ -194,32 +159,10 @@ int main()
         boost::fusion::at_c<0>(p) = 7;
         boost::fusion::at_c<1>(p) = 8;
         BOOST_TEST(p == boost::fusion::make_vector(7, 8));
-
-        BOOST_STATIC_ASSERT(
-            boost::fusion::result_of::size<pair_type>::value == 2
-        );
-        BOOST_STATIC_ASSERT(
-            !boost::fusion::result_of::empty<pair_type>::value
-        );
-
         BOOST_TEST(boost::fusion::front(p) == 7);
 #if 0
         BOOST_TEST(boost::fusion::back(p) == 8);
 #endif
-    }
-
-    {
-        boost::fusion::vector<int, float> v1(4, 3.3f);
-        boost::compressed_pair<short, float> v2(5, 3.3f);
-        boost::fusion::vector<long, double> v3(5, 4.4);
-        BOOST_TEST(v1 < v2);
-        BOOST_TEST(v1 <= v2);
-        BOOST_TEST(v2 > v1);
-        BOOST_TEST(v2 >= v1);
-        BOOST_TEST(v2 < v3);
-        BOOST_TEST(v2 <= v3);
-        BOOST_TEST(v3 > v2);
-        BOOST_TEST(v3 >= v2);
     }
 
     return boost::report_errors();
--- test/sequence/boost_container_pair.cpp
+++ test/sequence/boost_container_pair.cpp
@@ -5,126 +5,24 @@
     (See accompanying file LICENSE_1_0.txt or copy at
     http://www.boost.org/LICENSE_1_0.txt)
 ============================================================================*/
-#include <boost/core/lightweight_test.hpp>
 #include <boost/fusion/adapted/boost_container_pair.hpp>
-#include <boost/fusion/sequence/intrinsic/at.hpp>
-#include <boost/fusion/sequence/intrinsic/size.hpp>
-#include <boost/fusion/sequence/intrinsic/empty.hpp>
-#include <boost/fusion/sequence/intrinsic/front.hpp>
-#include <boost/fusion/sequence/intrinsic/back.hpp>
-#include <boost/fusion/sequence/io/out.hpp>
-#include <boost/fusion/container/vector/vector.hpp>
-#include <boost/fusion/container/list/list.hpp>
-#include <boost/fusion/container/generation/make_vector.hpp>
-#include <boost/fusion/container/vector/convert.hpp>
-#include <boost/fusion/sequence/comparison/equal_to.hpp>
-#include <boost/fusion/sequence/comparison/not_equal_to.hpp>
-#include <boost/fusion/sequence/comparison/less.hpp>
-#include <boost/fusion/sequence/comparison/less_equal.hpp>
-#include <boost/fusion/sequence/comparison/greater.hpp>
-#include <boost/fusion/sequence/comparison/greater_equal.hpp>
-#include <boost/fusion/mpl.hpp>
-#include <boost/fusion/support/is_view.hpp>
-#include <boost/mpl/is_sequence.hpp>
-#include <boost/mpl/front.hpp>
-#include <boost/mpl/back.hpp>
-#include <boost/mpl/at.hpp>
-#include <boost/mpl/assert.hpp>
 #include <boost/container/map.hpp>
-#include <iostream>
 #include <string>
+#include "adapted_pair.hpp"
 
 int main()
 {
-    using boost::fusion::operator<<;
-
-    std::cout << boost::fusion::tuple_open('[');
-    std::cout << boost::fusion::tuple_close(']');
-    std::cout << boost::fusion::tuple_delimiter(", ");
-
     {
-        typedef boost::container::map<
-            int
-          , std::string
-        >::movable_value_type pair_type;
-        BOOST_MPL_ASSERT((boost::mpl::is_sequence<pair_type>));
-        BOOST_MPL_ASSERT((
-            boost::is_same<int, boost::mpl::front<pair_type>::type>
-        ));
-        BOOST_MPL_ASSERT((
-            boost::is_same<std::string, boost::mpl::back<pair_type>::type>
-        ));
-        BOOST_MPL_ASSERT((
-            boost::is_same<int, boost::mpl::at_c<pair_type,0>::type>
-        ));
-        BOOST_MPL_ASSERT((
-            boost::is_same<std::string, boost::mpl::at_c<pair_type,1>::type>
-        ));
-        BOOST_MPL_ASSERT_NOT((boost::fusion::traits::is_view<pair_type>));
-
-        pair_type p(123, "Hola!!!");
-        std::cout << boost::fusion::at_c<0>(p) << std::endl;
-        std::cout << boost::fusion::at_c<1>(p) << std::endl;
-        std::cout << p << std::endl;
-        BOOST_TEST(p == boost::fusion::make_vector(123, "Hola!!!"));
-
-        boost::fusion::at_c<0>(p) = 6;
-        boost::fusion::at_c<1>(p) = "mama mia";
-        BOOST_TEST(p == boost::fusion::make_vector(6, "mama mia"));
-
-        BOOST_STATIC_ASSERT(
-            boost::fusion::result_of::size<pair_type>::value == 2
-        );
-        BOOST_STATIC_ASSERT(
-            !boost::fusion::result_of::empty<pair_type>::value
-        );
-
-        BOOST_TEST(boost::fusion::front(p) == 6);
+        boost::container::map<int, std::string>
+        ::movable_value_type p(123, "Hola!!!");
+        test::pair_access_and_modify(p);
         BOOST_TEST(boost::fusion::back(p) == "mama mia");
     }
 
     {
-        typedef boost::container::map<
-            short
-          , float
-        >::movable_value_type pair_type;
-        boost::fusion::vector<int, float> v1(4, 3.3f);
-        pair_type v2(5, 3.3f);
-        boost::fusion::vector<long, double> v3(5, 4.4);
-        BOOST_TEST(v1 < v2);
-        BOOST_TEST(v1 <= v2);
-        BOOST_TEST(v2 > v1);
-        BOOST_TEST(v2 >= v1);
-        BOOST_TEST(v2 < v3);
-        BOOST_TEST(v2 <= v3);
-        BOOST_TEST(v3 > v2);
-        BOOST_TEST(v3 >= v2);
-    }
-
-    {
-        // conversion from pair to vector
-        typedef boost::container::map<
-            int
-          , std::string
-        >::movable_value_type pair_type;
-        pair_type p(123, "Hola!!!");
-        boost::fusion::vector<int, std::string> v(p);
-        BOOST_TEST(v == p);
-        v = p;
-        BOOST_TEST(v == p);
-    }
-
-    {
-        // conversion from pair to list
-        typedef boost::container::map<
-            int
-          , std::string
-        >::movable_value_type pair_type;
-        pair_type p(123, "Hola!!!");
-        boost::fusion::list<int, std::string> l(p);
-        BOOST_TEST(l == p);
-        l = p;
-        BOOST_TEST(l == p);
+        boost::container::map<short, float>
+        ::movable_value_type p(5, 3.3f);
+        test::pair_compare(p);
     }
 
     return boost::report_errors();
