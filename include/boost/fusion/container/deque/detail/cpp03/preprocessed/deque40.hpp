ee82d3f0f01645240e0444dfdfefc0d4982637d6
CromwellEnage <32967088+CromwellEnage@users.noreply.github.com>
2018-10-10 12:34:43 -0400

    Fix bugs.

--- include/boost/fusion/container/deque/detail/cpp03/preprocessed/deque40.hpp
+++ include/boost/fusion/container/deque/detail/cpp03/preprocessed/deque40.hpp
@@ -1347,7 +1347,7 @@ deque(T_0 && t0 , T_1 && t1 , T_2 && t2 , T_3 && t3 , T_4 && t4 , T_5 && t5 , T_
             Sequence const& seq
           , typename ::boost::disable_if<
                 typename ::boost::mpl::if_<
-# if defined(BOOST_FUSION_USES_BOOST_VICE_CXX11_TYPE_TRAITS)
+# if defined(BOOST_FUSION_USES_BOOST_VICE_CXX11_TYPE_TRAITS) defined(BOOST_MSVC) && (BOOST_MSVC >= 1600) && (BOOST_MSVC < 1900) )
                     ::boost::is_convertible<Sequence, T0>
 # else
                     ::std::is_convertible<Sequence, T0>
@@ -1378,7 +1378,7 @@ deque(T_0 && t0 , T_1 && t1 , T_2 && t2 , T_3 && t3 , T_4 && t4 , T_5 && t5 , T_
             T0_&& t0
           , typename ::boost::enable_if<
                 typename ::boost::mpl::if_<
-# if defined(BOOST_FUSION_USES_BOOST_VICE_CXX11_TYPE_TRAITS)
+# if defined(BOOST_FUSION_USES_BOOST_VICE_CXX11_TYPE_TRAITS) defined(BOOST_MSVC) && (BOOST_MSVC >= 1600) && (BOOST_MSVC < 1900) )
                     ::boost::is_convertible<T0_, T0>
 # else
                     ::std::is_convertible<T0_, T0>
@@ -1390,7 +1390,7 @@ deque(T_0 && t0 , T_1 && t1 , T_2 && t2 , T_3 && t3 , T_4 && t4 , T_5 && t5 , T_
             >::type = ::boost::fusion::detail::enabler
           , typename ::boost::disable_if<
                 typename ::boost::mpl::if_<
-# if defined(BOOST_FUSION_USES_BOOST_VICE_CXX11_TYPE_TRAITS)
+# if defined(BOOST_FUSION_USES_BOOST_VICE_CXX11_TYPE_TRAITS) defined(BOOST_MSVC) && (BOOST_MSVC >= 1600) && (BOOST_MSVC < 1900) )
                     ::boost::is_same<
                         typename ::boost::remove_reference<T0_>::type const
 # else
@@ -1422,7 +1422,7 @@ deque(T_0 && t0 , T_1 && t1 , T_2 && t2 , T_3 && t3 , T_4 && t4 , T_5 && t5 , T_
             >&& seq
           , typename ::boost::disable_if<
                 typename ::boost::mpl::if_<
-# if defined(BOOST_FUSION_USES_BOOST_VICE_CXX11_TYPE_TRAITS)
+# if defined(BOOST_FUSION_USES_BOOST_VICE_CXX11_TYPE_TRAITS) defined(BOOST_MSVC) && (BOOST_MSVC >= 1600) && (BOOST_MSVC < 1900) )
                     ::boost::is_convertible<
 # else
                     ::std::is_convertible<
